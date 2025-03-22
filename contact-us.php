8c0344b9d9f121ccc402b9450375e61e41227c57
sib-vishal <vishal.maurya@sibinfo.tech>
2025-03-22 17:57:36 +0530

    commit architecture and contac t has been done

---
+++ contact-us.php
@@ -0,0 +1,383 @@
+<?php $page = 'home';
+
+$projects = [
+    [
+        "developer" => "JPV Realtors",
+        "location" => "Andheri West",
+        "status" => "Completed",
+        "design_team" => ["Dharana Shah", "Hiren Cholalia"],
+        "project_name" => "Pratap Elegance",
+        "category" => "Residential",
+        "images" => [
+            [
+                "thumbnail" => "https://horizonarchitects.co.in/wp-content/uploads/2025/02/6-scaled-500x450.jpg",
+                "full" => "https://horizonarchitects.co.in/wp-content/uploads/2025/02/6-1024x670.jpg"
+            ],
+            [
+                "thumbnail" => "https://horizonarchitects.co.in/wp-content/uploads/2025/02/5-scaled-500x450.jpg",
+                "full" => "https://horizonarchitects.co.in/wp-content/uploads/2025/02/5-1024x830.jpg"
+            ]
+        ]
+    ],
+    [
+        "developer" => "Rohan Reality",
+        "location" => "Malad",
+        "status" => "Completed",
+        "design_team" => ["Ar. Akshay Agarwal"],
+        "project_name" => "Satsang Bharti",
+        "category" => "Residential",
+        "images" => [
+            [
+                "thumbnail" => "https://horizonarchitects.co.in/wp-content/uploads/2025/02/7-scaled.jpg",
+                "full" => "https://horizonarchitects.co.in/wp-content/uploads/2025/02/7-scaled.jpg"
+            ]
+        ]
+    ],
+    [
+        "developer" => "Mukesh Mannat",
+        "location" => "Mulund",
+        "status" => "On Going",
+        "design_team" => ["Dharana Shah", "Hiren Chotalia"],
+        "project_name" => "Jaydeep Regalia",
+        "category" => "Residential",
+        "images" => [
+            [
+                "thumbnail" => "https://horizonarchitects.co.in/wp-content/uploads/2025/02/8-scaled-500x450.jpg",
+                "full" => "https://horizonarchitects.co.in/wp-content/uploads/2025/02/8-1024x661.jpg"
+            ],
+            [
+                "thumbnail" => "https://horizonarchitects.co.in/wp-content/uploads/2025/02/81-scaled-500x450.jpg",
+                "full" => "https://horizonarchitects.co.in/wp-content/uploads/2025/02/81-1024x816.jpg"
+            ],
+            [
+                "thumbnail" => "https://horizonarchitects.co.in/wp-content/uploads/2025/02/9-scaled-500x450.jpg",
+                "full" => "https://horizonarchitects.co.in/wp-content/uploads/2025/02/9-1024x808.jpg"
+            ],
+            [
+                "thumbnail" => "https://horizonarchitects.co.in/wp-content/uploads/2025/02/10-scaled-500x450.jpg",
+                "full" => "https://horizonarchitects.co.in/wp-content/uploads/2025/02/10-1024x621.jpg"
+            ]
+        ]
+    ],
+    [
+        "developer" => "Woodstock Reality",
+        "location" => "Khar",
+        "status" => "On Going",
+        "design_team" => ["Dharana Shah", "Hiren Chotalia"],
+        "project_name" => "Woodstock Reality",
+        "category" => "Residential",
+        "images" => [
+            [
+                "thumbnail" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/11-scaled.jpg",
+                "full" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/11-scaled.jpg"
+            ]
+        ]
+    ],
+    [
+        "developer" => "Terraform Reality",
+        "location" => "Wadala",
+        "status" => "On Going",
+        "design_team" => ["Hiren Chotalia"],
+        "project_name" => "Terraform Reality",
+        "category" => "Residential",
+        "images" => [
+
+            [
+                "thumbnail" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/12-scaled-500x500.jpg",
+                "full" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/12-1024x847.jpg"
+            ],
+            [
+                "thumbnail" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/13-scaled-500x500.jpg",
+                "full" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/13-1010x1024.jpg"
+            ],
+            [
+                "thumbnail" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/14-scaled-500x500.jpg",
+                "full" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/14-1024x781.jpg"
+            ]
+        ]
+    ],
+    [
+        "developer" => "Mangal Build Home",
+        "location" => "Vile Parle",
+        "status" => "Ongoing",
+        "design_team" => ["Dharana Shah", "Hiren Chotalia", "Dhwani Vaghasiya"],
+        "project_name" => "Mangal Signature",
+        "category" => "Residential",
+        "images" => [
+            [
+                "thumbnail" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/15-scaled-600x550.jpg",
+                "full" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/15-scaled-600x550.jpg"
+            ]
+        ]
+    ],
+    [
+        "developer" => "Bhatia Builders",
+        "location" => "Borivali",
+        "status" => "Ongoing",
+        "design_team" => ["Dharana Shah", "Hiren Chotalia", "Dhwani Vaghasiya"],
+        "project_name" => "Espee Square",
+        "category" => "Residential",
+        "images" => [
+            [
+                "thumbnail" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/16-scaled.jpg",
+                "full" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/16-scaled.jpg"
+            ]
+        ]
+    ],
+    [
+        "developer" => "Atul Builders",
+        "location" => "Bhandup",
+        "status" => "Ongoing",
+        "design_team" => ["Dharana Shah", "Hiren Chotalia"],
+        "project_name" => "Atul Horizon",
+        "category" => "Commercial",
+        "images" => [
+            [
+                "thumbnail" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/17-scaled.jpg",
+                "full" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/17-scaled.jpg"
+            ]
+        ]
+    ],
+    [
+        "developer" => "Mukesh Minat",
+        "location" => "Malad",
+        "status" => "Ongoing",
+        "design_team" => ["Dharana Shah", "Hiren Chotalia", "Dhwani Vaghasiya"],
+        "project_name" => "Gami Group Ventura Commercial Hub",
+        "category" => "Commercial",
+        "images" => [
+            [
+                "thumbnail" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/19-scaled-500x420.jpg",
+                "full" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/19-1024x608.jpg"
+            ],
+            [
+                "thumbnail" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/18-scaled-500x420.jpg",
+                "full" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/18-1024x878.jpg"
+            ]
+        ]
+    ],
+    [
+        "developer" => "Daiwwik Group",
+        "location" => "Goregaon",
+        "status" => "Ongoing",
+        "design_team" => ["Dharana Shah", "Hiren Chotalia"],
+        "project_name" => "Young Man Society",
+        "category" => "Commercial",
+        "images" => [
+            [
+                "thumbnail" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/20-scaled-500x420.jpg",
+                "full" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/20-1024x791.jpg"
+            ],
+            [
+                "thumbnail" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/21-scaled-500x420.jpg",
+                "full" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/21-1024x616.jpg"
+            ]
+        ]
+    ],
+    [
+        "developer" => "Ossia Developers",
+        "location" => "Borivali",
+        "status" => "Ongoing",
+        "design_team" => ["Dharana Shah", "Hiren Chotalia", "Dhwani Vaghasiya"],
+        "project_name" => "Shiv Sadan",
+        "category" => "Commercial",
+        "images" => [
+            [
+                "thumbnail" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/23-scaled-500x420.jpg",
+                "full" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/23-1024x699.jpg"
+            ],
+            [
+                "thumbnail" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/22-scaled-500x420.jpg",
+                "full" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/22-1024x847.jpg"
+            ]
+        ]
+    ],
+    [
+        "developer" => "Madhu Developers",
+        "location" => "Goregaon",
+        "status" => "Ongoing",
+        "design_team" => ["Dharana Shah", "Hiren Chotalia", "Dhwani Vaghasiya"],
+        "project_name" => "Madhu Estate",
+        "category" => "Commercial",
+        "images" => [
+            [
+                "thumbnail" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/24-scaled.jpg",
+                "full" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/24-scaled.jpg"
+            ]
+        ]
+    ],
+    [
+        "developer" => "Mangal Build Home",
+        "location" => "Chembur",
+        "status" => "Completed",
+        "design_team" => ["Dharana Shah", "Hiren Chotalia", "Dhwani Vaghasiya"],
+        "project_name" => "Mangal Smriti",
+        "category" => "Mixed Use",
+        "images" => [
+            [
+                "thumbnail" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/1-scaled-500x420.jpg",
+                "full" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/1-802x1024.jpg"
+            ],
+            [
+                "thumbnail" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/2-scaled-500x420.jpg",
+                "full" => "https://horizonarchitects.co.in/wp-content/uploads/2025/03/2-857x1024.jpg"
+            ]
+        ]
+    ]
+];
+
+
+
+
+$chunks = array_chunk($projects, ceil(count($projects) / 3));
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Horizon Architects - Welcome to Our Website </title>
+    <?php include 'include/head-links.php'; ?>
+    <meta property="og:url" content="<?php echo $page_url ?>">
+    <meta property="og:title" content="">
+    <meta property="og:description" content="">
+    <meta property="og:image" content="images/logo.png">
+    <meta name="twitter:card" content="">
+    <meta name="twitter:site" content="<?php echo $page_url ?>">
+    <meta name="twitter:title" content="">
+    <meta name="twitter:description" content="">
+    <meta name="twitter:image" content="images/logo.png">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
+</head>
+
+<body onload="createCaptcha();">
+    <div class="wrapper ">
+        <?php include 'include/header.php'; ?>
+
+        <div class="banner_page  ">
+            <div class="containerFull">
+                <div class="inner_banner_page ">
+                    <div class="inner_banner_page_content">
+
+                        <h4 class="heading fontHeading fontWeight600">
+                            Contact Us --
+                        </h4>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+        <section>
+            <div class="containerFull">
+                <div class="row">
+                    <div class="col-lg-5">
+                        <div class="leftContactInnerPage">
+                            <div class="d-flex align-items-center">
+                                <div class="iconLaw">
+                                    <i class="fa-solid fa-house"></i>
+                                </div>
+                                <div class="rightIconLaw">
+                                    <p class="title fontWeight600 text_primary"><i>Get in Touch</i></p>
+                                </div>
+                            </div>
+                            <h2 class="sub_heading fontWeight600 mt-4">Do you need help?</h2>
+                            <h3 class="sub_heading fontWeight600 text_primary mt-2"><i>Contact with us now!</i></h3>
+                            <div class="newContactIcons stickyItem">
+                                <div class="connectIcon">
+                                    <div class="newIcons">
+                                        <div class="loader">
+                                            <i class="fa-solid fa-location-dot"></i>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <h3 class="fontWeight600 fontHeading">Address</h3>
+                                        <h5>Topiwala Centre, 2ND FLOOR, OFFICE UNIT NO.1, Washington Plaza, Swami Vivekanand Marg, Goregaon West, Mumbai Suburban, MH-400104</h5>
+                                    </div>
+                                </div>
+                                <div class="connectIcon">
+                                    <div class="newIcons">
+                                        <div class="loader">
+                                            <i class="fa-solid fa-envelope"></i>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <h3 class="fontWeight600 fontHeading">Phone</h3>
+
+                                        <h5><a class="text-black" href="tel: 022- 35110154"> 022 2613 6658
+                                            </a></h5>
+                                    </div>
+                                </div>
+                                <div class="connectIcon">
+                                    <div class="newIcons">
+                                        <div class="loader">
+                                            <i class="fa-solid fa-phone"></i>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <h3 class="fontWeight600 fontHeading">Email</h3>
+
+                                        <h5><a class="text-black"
+                                                href="mailto:info@horizonarchitects.co.in">info@horizonarchitects.co.in</a></h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-5 offset-lg-2">
+                        <div class="rightContactInner">
+                            <h3 class="sub_heading fontHeading fontWeight60 text-center"><span
+                                    class="text-white">Contact
+                                    with us</span> <span class="text_primary">now !</span></h3>
+                            <form action="" class="mt-4">
+                                <div class="itemFormInner">
+                                    <input type="text" name="name" placeholder="Full Name" required>
+                                </div>
+                                <div class="itemFormInner">
+                                    <input type="email" name="email" placeholder="Email Address" required>
+                                </div>
+                                <div class="itemFormInner">
+                                    <input type="phone" name="phone" placeholder="Phone Number" required>
+                                </div>
+                                <div class="itemFormInner">
+                                    <input type="text" name="company" placeholder="Company Name">
+                                </div>
+                                <!--<div class="itemFormInner">-->
+                                <!--    <textarea name="message" id="" placeholder="Write your message"></textarea>-->
+                                <!--</div>-->
+                                <div class="itemFormInner">
+                                    <button class="btnContact">Send Message</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="pt-0">
+            <div class="containerFull">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d25164.100081752367!2d72.846702!3d19.163451!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c9b41b55c2cf%3A0xdcac34199d7f880b!2sHorizon%20Architects!5e1!3m2!1sen!2sin!4v1742645837940!5m2!1sen!2sin"
+                    width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+            </div>
+        </section>
+    </div>
+
+
+
+    <?php include 'include/footer.php'; ?>
+
+    </div>
+    <?php include 'include/footer-links.php'; ?>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+</body>
+
+</html>
